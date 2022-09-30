6631b88da37578a88378393dd8d3f143e2be0579
Victoria R <vik-rozanova@yandex.ru>
2022-10-01 00:11:41 +0300

    add all for the program code

---
+++ MainProgram/Program.cs
@@ -0,0 +1,44 @@
+﻿void PrintArray(string[] array)
+{
+    int i;
+    for (i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+
+    Console.WriteLine(array[i]);
+}
+
+string[] ElementsLengthThreeAndLessArray(string[] array)
+{
+    int size = 0;
+    string[] newArray = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Array.Resize(ref newArray, newArray.Length + 1);
+            newArray[size] = array[i];
+            size++;
+        }
+    }
+    string[] emptyResult = new string[] {"there are no such values", "try again"};
+    if(newArray.Length == 0) return emptyResult;
+    else return newArray;
+}
+
+string[] first = new string[] { "hello", "2", "world", ":-)" };
+PrintArray(first);
+Console.Write($"Result: ");
+PrintArray(ElementsLengthThreeAndLessArray(first));
+
+string[] second = new string[] { "1234", "1567", "-2", "computer science" };
+PrintArray(second);
+Console.Write($"Result: ");
+
+PrintArray(ElementsLengthThreeAndLessArray(second));
+
+string[] third = new string[] { "Russia", "Denmark", "Kazan"};
+PrintArray(third);
+Console.Write($"Result: ");
+PrintArray(ElementsLengthThreeAndLessArray(third));
